4c8a5cc1f15e401f14921745c500631713c826bd
Martin Giger <martin.a.giger@sunrise.ch>
2015-05-18 17:42:42 +0200

    Fix some stuff

--- index.js
+++ index.js
@@ -3,32 +3,45 @@
  */
 
 var http = require("http");
-var save-to = "require("save-to");
+var saveto = require("save-to");
 var JSFtp = require("jsftp");
 
 var path = "/pub/mozilla.org/mobile/nightly/latest-mozilla-central-android-api-11/";
 
 exports.getNightlyLocation = function(cbk) {
-    var ftp = JSFtp({
+    var ftp = new JSFtp({
         host: "ftp.mozilla.org"
     });
-    ftp.ls(path, function(err, res) {
-        res.some(function(file) {
-            if(file.name.match(/fennec-.*\.apk/)) {
-                cbk(path + file.name);
-                return true;
-            }
-            else {
-                return false;
-            }
+    ftp.auth(null, null, function(e) {
+        if(e) process.stdout.write(e);
+        ftp.ls(path, function(err, res) {
+            if(err)  process.stdout.write(err);
+            res.some(function(file) {
+                if(file.name.match(/fennec-.*\.apk/)) {
+                    cbk(path + file.name);
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            });
         });
     });
 };
 
 exports.downloadNightly = function(destination, cbk) {
     exports.getNightlyLocation(function(source) {
-        http.get("https://ftp.mozilla.org"+source, function(rsp) {
-            save-to(rsp, destination, cbk);
+        process.stdout.write("Downloading from http://ftp.mozilla.org" + source + "\n");
+        var req = http.get("http://ftp.mozilla.org"+source, function(rsp) {
+            saveto(rsp, destination, function(e, d) {
+                if(e) process.stdout.write(e);
+                else process.stdout.write("Saved to " + destination + "\n");
+                cbk(e, d);
+            });
+        });
+
+        req.on("error", function(e) {
+            process.stdout.write(e);
         });
     });
 };

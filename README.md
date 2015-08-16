4b2536f71c38d318381f4edf5776d7cf6d4f98f5
Martin Giger <martin.a.giger@sunrise.ch>
2015-08-16 18:39:30 +0200

    get-firefox

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# get-fennec
+# get-firefox
 
 ## Installation
 
-Install this command by running `npm install -g get-fennec`
+Install this command by running `npm install -g get-firefox`.
 
 ## Usage
-Run `get-fennec fennec.apk` in the console to download the latest Firefox for Android nightly as the file "fennec.apk".
+Run `get-firefox --platform android --target fennec.apk` in the console to download the latest Firefox nightly for Android as `fennec.apk`. Currently downloads 64-bit versions only, when there is a choice.
--- index.js
+++ index.js
@@ -1,27 +1,57 @@
 /*
  * Licensed under the MPL-2.0
  */
+//TODO add architecture choice support
 
 var fs = require("fs");
 var request = require("request");
 
 var URL = "https://archive.mozilla.org";
+var BASE_PATH = "/pub/mozilla.org/";
 
-var path = "/pub/mozilla.org/mobile/nightly/latest-mozilla-central-android-api-11/";
+var PLATFORMS = {
+    android: {
+        path: "mobile/nightly/latest-mozilla-central-android-api-11/",
+        fileEnding: "apk",
+        substringLength: ("common.tests.zip").length
+    },
+    linux: {
+        path: "firefox/nightly/latest-trunk/",
+        fileEnding: "linux-x86_64.tar.bz2",
+        substringLength: ("win64.common.tests.zip").length
+    },
+    mac: {
+        path: "firefox/nightly/latest-trunk/",
+        fileEnding: "mac.dmg",
+        substringLength: ("win64.common.tests.zip").length
+    },
+    win: {
+        path: "firefox/nightly/latest-trunk/",
+        fileEnding: "installer.exe",
+        substringLength: ("common.tests.zip").length
+    }
+};
 
-exports.getNightlyLocation = function(cbk) {
-    request(URL+path+"test_packages.json", function(error, response, body) {
+exports.getNightlyLocation = function(platform, cbk) {
+    platform = platform || "linux";
+    var platformObj = PLATFORMS[platform];
+    request(URL+BASE_PATH+platformObj.path+"test_packages.json", function(error, response, body) {
         if(!error && response.statusCode == 200) {
             var json = JSON.parse(body);
-            cbk(json.common[0].substr(0,  json.common[0].length-("common.tests.zip").length)+"apk");
+            cbk(platformObj.path
+                + json.common[0].substr(0,
+                      json.common[0].length-platformObj.substringLength)
+                + platformObj.fileEnding);
         }
     });
 };
 
-exports.downloadNightly = function(destination, cbk) {
-    exports.getNightlyLocation(function(source) {
-        process.stdout.write("Downloading from "+ URL + path + source + " and saving to " + destination + "\n");
-        var req = request(URL+path+source).pipe(fs.createWriteStream(destination));
+exports.downloadNightly = function(platform, destination, cbk) {
+    exports.getNightlyLocation(platform, function(source) {
+        destination = destination || source.split("/").pop();
+
+        process.stdout.write("Downloading from "+ URL + BASE_PATH + source + " and saving to " + destination + "\n");
+        var req = request(URL+BASE_PATH+source).pipe(fs.createWriteStream(destination));
 
         req.on("response", cbk);
 

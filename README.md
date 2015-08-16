b59350c69cd25d89145aa09e8858a40629dc65a6
Martin Giger <martin.a.giger@sunrise.ch>
2015-08-17 01:03:06 +0200

    0.0.3

--- README.md
+++ README.md
@@ -1,8 +1,42 @@
 # get-firefox
+Download the latest Firefox from the command line. Currently only desktop and
+android nightlies.
+
+Note that this package works even after the recent ftp.mozilla.org to
+[archive.mozilla.org][] change.
+
+It determines the file names using a helperfile built with the other packages,
+so it might break if the build infrastructure breaks.
 
 ## Installation
 
 Install this command by running `npm install -g get-firefox`.
 
-## Usage
-Run `get-firefox --platform android --target fennec.apk` in the console to download the latest Firefox nightly for Android as `fennec.apk`. Currently downloads 64-bit versions only, when there is a choice.
+## Usage in the command line
+Run `get-firefox --platform android --target fennec.apk --architecture arm-v11` in the console to download the latest Firefox nightly for Android as `fennec.apk`. Currently downloads 64-bit versions only, when there is a choice.
+
+Run `get-firefox --help` for a complete description of possible options.
+
+## Usage as node module
+```js
+var getFirefox = require("get-firefox");
+```
+The main module implements two public methods.
+
+### getNightlyLocation(platform, architecture)
+Returns a promise that resolves with an URL pointing to the file to download.
+
+### downloadNightly(source, destination, progressCallback)
+Downloads a file from source and saves it in destination. Returns a promise.
+The progress callback is called with a state object with the following structure:
+```js
+{
+  total: 1000, // Total size in bytes
+  received: 500, // Received size in bytes
+  percent: 0.5 // Percentage received
+}
+```
+
+## License
+The source code in this package is licensed under the MPL-2.0. A copy of the
+license text can be found in the [LICENSE][] file.
--- index.js
+++ index.js
@@ -1,31 +1,72 @@
 /*
  * Licensed under the MPL-2.0
  */
-//TODO add architecture & api version choice support
-//TODO verify signatures stuff
-//TODO autoextract?
+
+//TODO win zips vs. installers
+//TODO multiple release channels/branches
+//TODO verify checksum file signature...
 
 var fs = require("fs");
 var request = require("request");
+var progress = require("request-progress");
+var Promise = require("es6-promise").Promise;
+var checksum = require("checksum");
+var Decompress = require("decompress");
 
 var URL = "https://archive.mozilla.org/pub/";
 
 var PLATFORMS = {
     android: {
-        path: "mobile/nightly/latest-mozilla-central-android-api-11/",
-        fileEnding: ".android-arm.apk"
+        defaultArch: "arm-v11",
+        arches: {
+            "arm-v11": {
+                path: "mobile/nightly/latest-mozilla-central-android-api-11/",
+                fileEnding: ".android-arm.apk"
+            },
+            "arm-v9": {
+                path: "mobile/nightly/latest-mozilla-central-android-api-9/",
+                fileEnding: ".android-arm.apk"
+            },
+            "x86": {
+                path: "mobile/nightly/latest-mozilla-entral-android-x86/",
+                fileEnding: ".android-i386.apk"
+            }
+        }
     },
     linux: {
-        path: "firefox/nightly/latest-trunk/",
-        fileEnding: ".linux-x86_64.tar.bz2"
+        defaultArch: "x86_64",
+        arches: {
+            "x86_64": {
+                path: "firefox/nightly/latest-trunk/",
+                fileEnding: ".linux-x86_64.tar.bz2"
+            },
+            "x86": {
+                path: "firefox/nightly/latest-trunk/",
+                fileEnding: ".linux-i686.tar.bz2"
+            }
+        }
     },
     mac: {
-        path: "firefox/nightly/latest-trunk/",
-        fileEnding: ".mac.dmg"
+        defaultArch: "multi",
+        arches: {
+            "multi": {
+                path: "firefox/nightly/latest-trunk/",
+                fileEnding: ".mac.dmg"
+            }
+        }
     },
     win: {
-        path: "firefox/nightly/latest-trunk/",
-        fileEnding: ".win64.installer.exe"
+        defaultArch: "x86_64",
+        arches: {
+            "x86_64": {
+                path: "firefox/nightly/latest-trunk/",
+                fileEnding: ".win64.installer.exe"
+            },
+            "x86": {
+                path: "firefox/nightly/latest-trunk/",
+                fileEnding: ".win32.installer.exe"
+            }
+        }
     }
 };
 
@@ -33,30 +74,154 @@ var getBaseFilenameFromTestPackage = function(testPackageName) {
     return testPackageName.split(".").slice(0, 3).join(".");
 };
 
-exports.getNightlyLocation = function(platform, cbk) {
-    platform = platform || "linux";
-    var platformObj = PLATFORMS[platform];
-    request(URL+platformObj.path+"test_packages.json", function(error, response, body) {
-        if(!error && response.statusCode == 200) {
-            var json = JSON.parse(body);
-            cbk(platformObj.path
-                + getBaseFilenameFromTestPackage(json.common[0])
-                + platformObj.fileEnding);
+var getDownloadInfo = function(system, arch) {
+    if(!(system in PLATFORMS)) {
+        console.warn("Unknown platform '"+system+"', defaulting to linux");
+        system = "linux";
+    }
+    var systemObj = PLATFORMS[system];
+    if(!(arch in systemObj.arches)) {
+        if(arch) {
+            console.warn("There is no architecture "+arch+" for "+system+", defaulting to "+systemObj.defaultArch);
         }
+        arch = systemObj.defaultArch;
+    }
+    return systemObj.arches[arch];
+};
+
+var getArchForURL = function(source) {
+    for(var system in PLATFORMS) {
+        for(var a in PLATFORMS[system].arches) {
+            if(source.indexOf(PLATFORMS[system].arches[a].fileEnding) != -1) {
+                return PLATFORMS[system].arches[a];
+            }
+        }
+    }
+};
+
+var getChecksumFileName = function(source) {
+    var arch = getArchForURL(source);
+
+    var purgeLength = arch.fileEnding.split(".").length - 2;
+
+    return source.split(".").slice(0, -purgeLength).join(".") + ".checksums";
+};
+
+var getChecksumFile = function(source) {
+    return new Promise(function(resolve, reject) {
+        request(getChecksumFileName(source), function(error, response, body) {
+            if(!error && response.statusCode == 200) {
+                resolve(body);
+            }
+            else {
+                reject(error);
+            }
+        });
     });
 };
 
-exports.downloadNightly = function(platform, destination, cbk) {
-    exports.getNightlyLocation(platform, function(source) {
-        destination = destination || source.split("/").pop();
+var getFileChecksum = function(source) {
+    return getChecksumFile(source).then(function(file) {
+        var fileName = source.split("/").pop();
+        var sum, meta;
+        file.split("\n").some(function(line) {
+            meta = line.split(" ");
+            if(meta[1] == "sha512" && meta[3].split("/").pop() == fileName) {
+                sum = meta[0];
+                return true;
+            }
+            return false;
+        });
+        if(!sum)
+            throw "Could not find a checksum for "+fileName;
+        else
+            return sum;
+    });
+};
 
-        process.stdout.write("Downloading from "+ URL + source + " and saving to " + destination + "\n");
-        var req = request(URL+source).pipe(fs.createWriteStream(destination));
+var getDecompressPlugin = function(source) {
+    var ext = source.split(".").pop();
+    if(ext == "bz2") {
+        return Decompress.tarbz2();
+    }
+    else if(ext == "zip") {
+        return Decompress.zip();
+    }
+    else if(ext == "gz") {
+        return Decompress.targz();
+    }
+    else if(ext == "tar") {
+        return Decompress.tar();
+    }
+};
 
-        req.on("response", cbk);
+exports.getNightlyLocation = function(platform, arch) {
+    platform = platform || "linux";
+    var platformObj = getDownloadInfo(platform, arch);
 
-        req.on("error", function(e) {
-            process.stdout.write(e);
+    return new Promise(function(resolve, reject) {
+        request(URL+platformObj.path+"test_packages.json", function(error, response, body) {
+            if(!error && response.statusCode == 200) {
+                var json = JSON.parse(body);
+                resolve(URL
+                    + platformObj.path
+                    + getBaseFilenameFromTestPackage(json.common[0])
+                    + platformObj.fileEnding
+                );
+            }
+            else {
+                reject("Couldn't load the information file "+URL+platformObj.path+"test_packages.json");
+            }
         });
     });
+};
+
+exports.downloadNightly = function(source, destination, progressCbk) {
+    return new Promise(function(resolve, reject) {
+        progress(request(source))
+        .on("error", reject)
+        .on("progress", progressCbk)
+        .pipe(fs.createWriteStream(destination))
+        .on("error", reject)
+        .on("close", resolve);
+    });
+};
+
+exports.check = function(source, localName) {
+    return Promise.all([
+        getFileChecksum(source),
+        new Promise(function(resolve, reject) {
+            checksum.file(localName, { algorithm: 'sha512' }, function(e, cs) {
+                if(!e)
+                    resolve(cs);
+                else
+                    reject(e);
+            });
+        })
+    ]).then(function(c) {
+        if(c[0] == c[1])
+            return "Checksum matches";
+        else
+            throw "Checksum mismatch";
+    });
+};
+
+exports.extract = function(file) {
+    if(file.indexOf(".dmg") == -1) {
+        return new Promise(function(resolve, reject) {
+            new Decompress()
+            .src(file)
+            .dest('.')
+            .use(getDecompressPlugin(file))
+            .run(function(e) {
+                if(!e)
+                    resolve();
+                else
+                    reject();
+            });
+        });
+    }
+    else {
+        //TODO extract dmg. But I'm too lazy to implement that. There's a dmg module out there.
+    }
 };

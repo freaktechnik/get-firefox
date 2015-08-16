15274ac43c8894dea8d7db3fb7547b52df7ada40
Martin Giger <martin.a.giger@sunrise.ch>
2015-08-16 19:58:26 +0200

    2.0.0 - better json parsing

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 /*
  * Licensed under the MPL-2.0
  */
-//TODO add architecture choice support
+//TODO add architecture & api version choice support
+//TODO verify signatures stuff
+//TODO autoextract?
 
 var fs = require("fs");
 var request = require("request");
 
-var URL = "https://archive.mozilla.org";
-var BASE_PATH = "/pub/mozilla.org/";
+var URL = "https://archive.mozilla.org/pub/";
 
 var PLATFORMS = {
     android: {
         path: "mobile/nightly/latest-mozilla-central-android-api-11/",
-        fileEnding: "apk",
-        substringLength: ("common.tests.zip").length
+        fileEnding: ".android-arm.apk"
     },
     linux: {
         path: "firefox/nightly/latest-trunk/",
-        fileEnding: "linux-x86_64.tar.bz2",
-        substringLength: ("win64.common.tests.zip").length
+        fileEnding: ".linux-x86_64.tar.bz2"
     },
     mac: {
         path: "firefox/nightly/latest-trunk/",
-        fileEnding: "mac.dmg",
-        substringLength: ("win64.common.tests.zip").length
+        fileEnding: ".mac.dmg"
     },
     win: {
         path: "firefox/nightly/latest-trunk/",
-        fileEnding: "installer.exe",
-        substringLength: ("common.tests.zip").length
+        fileEnding: ".win64.installer.exe"
     }
+};
+
+var getBaseFilenameFromTestPackage = function(testPackageName) {
+    return testPackageName.split(".").slice(0, 3).join(".");
 };
 
 exports.getNightlyLocation = function(platform, cbk) {
     platform = platform || "linux";
     var platformObj = PLATFORMS[platform];
-    request(URL+BASE_PATH+platformObj.path+"test_packages.json", function(error, response, body) {
+    request(URL+platformObj.path+"test_packages.json", function(error, response, body) {
         if(!error && response.statusCode == 200) {
             var json = JSON.parse(body);
             cbk(platformObj.path
-                + json.common[0].substr(0,
-                      json.common[0].length-platformObj.substringLength)
+                + getBaseFilenameFromTestPackage(json.common[0])
                 + platformObj.fileEnding);
         }
     });
@@ -50,8 +50,8 @@ exports.downloadNightly = function(platform, destination, cbk) {
     exports.getNightlyLocation(platform, function(source) {
         destination = destination || source.split("/").pop();
 
-        process.stdout.write("Downloading from "+ URL + BASE_PATH + source + " and saving to " + destination + "\n");
-        var req = request(URL+BASE_PATH+source).pipe(fs.createWriteStream(destination));
+        process.stdout.write("Downloading from "+ URL + source + " and saving to " + destination + "\n");
+        var req = request(URL+source).pipe(fs.createWriteStream(destination));
 
         req.on("response", cbk);
 

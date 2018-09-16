4bd806bc82fa4e25523f0b07b60aa168d9cb3964
Martin Giger <martin@humanoids.be>
2018-09-16 15:45:22 +0200

    tests: clean up get-firefox-bin test

--- test/get-firefox-bin.js
+++ test/get-firefox-bin.js
@@ -2,32 +2,38 @@ import test from 'ava';
 import tmp from 'tmp-promise';
 import path from 'path';
 import util from 'util';
-import child_process from 'child_process';
-import fs from 'fs-extra';
+import childProcess from 'child_process';
+import fs from 'fs';
+import PLATFORMS from '../lib/platforms.json';
 
-const exec = util.promisify(child_process.exec);
-tmp.setGracefulCleanup();
+const exec = util.promisify(childProcess.exec);
+const readDir = util.promisify(fs.readdir);
+const stat = util.promisify(fs.stat);
 
-const getFirefox = async(t, cliParams = '') => {
-  const getFirefoxBinary = path.resolve('./bin/get-firefox');
-  const tmpDir = await tmp.dir({ unsafeCleanup: true });
-  const { stdout, stderr } = await exec(`${getFirefoxBinary} ${cliParams}`, { shell: true, cwd: tmpDir.path });
-  t.is(stderr, '');
-  const files = await fs.readdir(tmpDir.path);
-  t.is(files.length, 1, "One file was downloaded");
-  const filePath = path.join(tmpDir.path, files[0]);
-  const stats = await fs.stat(filePath);
-  t.is(stats.size > 0, true, "The file has a size greater than 0");
+test.before(() => {
+    tmp.setGracefulCleanup();
+});
+
+const testGetFirefox = async (t, platform) => {
+    const getFirefoxBinary = path.resolve('./bin/get-firefox');
+    const tmpDir = await tmp.dir({ unsafeCleanup: true });
+    const { stderr } = await exec(`${getFirefoxBinary} -p ${platform}`, {
+        shell: true,
+        cwd: tmpDir.path
+    });
+    t.is(stderr, '');
+    const files = await readDir(tmpDir.path);
+    t.is(files.length, 1, "One file was downloaded");
+    const filePath = path.join(tmpDir.path, files[0]);
+    const stats = await stat(filePath);
+    t.is(stats.size > 0, true, "The file has a size greater than 0");
+
+    tmpDir.cleanup();
 };
+testGetFirefox.title = (title, platform) => `${title}: ${platform}`;
 
-test('linux', async(t) => {
-  await getFirefox(t, '-p linux');
-});
-
-test('mac', async(t) => {
-  await getFirefox(t, '-p mac');
-});
-
-test('windows', async(t) => {
-  await getFirefox(t, '-p win');
-});
+for(const platform in PLATFORMS) {
+    if(PLATFORMS.hasOwnProperty(platform)) {
+        test('platform downloads something', testGetFirefox, platform);
+    }
+}

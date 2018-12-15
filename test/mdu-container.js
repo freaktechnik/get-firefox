f0ca764101e0913ecad3462482ae7f25748a3a4e
Martin Giger <martin@humanoids.be>
2018-12-15 22:06:04 +0100

    chore: upgrade tests

--- test/mdu-container.js
+++ test/mdu-container.js
@@ -11,5 +11,5 @@ test("MDUContainer interface check", testContainer, MDUContainer, {
 test("Get Checksums rejects", (t) => {
     const container = new MDUContainer({});
 
-    return t.throws(container.getChecksums(), Error, "No checksums available");
+    return t.throwsAsync(container.getChecksums(), Error, "No checksums available");
 });
